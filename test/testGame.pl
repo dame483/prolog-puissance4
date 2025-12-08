06e0d0a759767084d575b401ea344112f8f4c3fd
KML-Style <nathandd88@free.fr>
2025-12-08 08:57:42 +0100

    ✨ add  other games functions

--- test/testGame.pl
+++ test/testGame.pl
@@ -38,7 +38,31 @@ test_board4([
     []      
 ]).
 
+test_board5([
+    ['x','o','x','o'],      
+    ['o','o','x','x'],      
+    ['x','x','o'],      
+    ['x','o','x','x'],      
+    ['o','x','o','o','o'],      
+    [],      
+    []      
+]).
+
+test_board6([
+    ['x','o','x','o','x','o'],      
+    ['o','x','o','x','o','x'],      
+    ['x','o','x','o','x','o'],      
+    ['x','o','x','o','x','o'],      
+    ['o','x','o','x','o','x'],      
+    ['x','o','x','o','x','o'],      
+    ['x','o','x','o','x','o']
+]).
+
 % ?- test_board4(B), diagonal_win_down(B, 'o').
 % ?- test_board3(B), diagonal_win_up(B, 'x').
 % ?- test_board2(B), vertical_win(B,'o').
 % ?- test_board2(B), horizontal_win(B, 'x').
+
+% ?- test_board3(B), game_over(B,Winner).
+% ?- test_board4(B), game_over(B,Winner).
+% ?- test_board5(B), game_over(B,Winner).
